7e00bf41b0885baf686efc74053409b5f09e0055
Ha Trong Nghia <hatrongnghia@outlook.com>
2021-12-04 19:29:28 +0700

    Finish day 001

---
+++ R1/D001/brand-name-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = input("What is the city you was born?\n")
+pet = input("What is your pet's name?\n")
+print("The band name is: " + city + " " + pet)
--- R1/README.md
+++ R1/README.md
@@ -1,13 +1,12 @@
 # Round 1
 
 ## Start date
-
 4th December 2021
 
 ## Language
-
 Python
 
 ## Resources
-
 100 Days of Code - The Complete Python Pro Bootcamp for 2021
+
+### Day 001
