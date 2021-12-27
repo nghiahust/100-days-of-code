70274e83ac896f3a9cb9dbd73d64de7941eaeefe
Ha Trong Nghia <nghiaxsm@MyArch.localdomain>
2021-12-28 04:05:34 +0700

    Backup

---
+++ R1/D019/turtle-racing.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+import random
+
+my_turtle = Turtle()
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title='Make your bet', prompt= 'Which turtle will
+win the race? Enter a color: ')
+
+
+my_turtle = Turtle()
+
+
+
+
+screen.exitonclick()
